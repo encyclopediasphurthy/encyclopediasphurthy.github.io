6c3394212570404bb48f649eb9cb114b005e6a47
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-04 18:48:19 -0400

    Update az.js

--- az.js
+++ az.js
@@ -1,1 +1,16 @@
-
+fetch('az.json')
+  .then(response => {
+    return response.json();
+  })
+  .then(json=>{
+        console.log(json);
+        var data = json;
+        /*var place_1 = document.querySelectorAll(".place");
+        var place = place_1[0];
+        console.log(place);
+        var fact = data.fact;
+        console.log(fact);
+        place.innerHTML = fact;
+        var place_2 = document.getElementById("txt");
+        place_2.innerHTML = data.source;*/
+  })
