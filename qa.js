4f38496e825f20042ae341fd5a8989ae786fa73e
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-06 18:47:17 -0400

    Update qa.js

--- qa.js
+++ qa.js
@@ -1,7 +1,4 @@
 function load()
 {
-window.open('http://www.google.com','_blank');
-window.open('http://www.yahoo.com','_blank');
+window.open('mailto:encyclopediasphurthy@gmail.com','_blank');
 }
-
-load();
