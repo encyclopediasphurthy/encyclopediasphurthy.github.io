c3cbf4e752eccbc074a9da6915e15d769b6b9d06
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-10 16:41:39 -0400

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,6 +1,5 @@
-var feedback = document.getElementById("feedback");
-
 function load()
 {
-window.open('mailto:encyclopediasphurthy@gmail.com?bcc=jakkipally@gmail.com&subject=Feedback to Sphurthy Encyclopedia&body= + feedback!','_blank');
+var feedback = document.getElementById("feedback");
+window.open('mailto:encyclopediasphurthy@gmail.com?bcc=jakkipally@gmail.com&subject=Feedback to Sphurthy Encyclopedia&body= + feedback','_blank');
 }
