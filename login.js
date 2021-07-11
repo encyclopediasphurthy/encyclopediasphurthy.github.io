20bf37ee3898a59d4aff80704f844cada3b94a2a
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-11 16:24:33 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,13 @@
+function close() {
+  document.getElementById('id01').style.display='none'
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
