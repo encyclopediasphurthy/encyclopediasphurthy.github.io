5462231cddf2cb8294b8660366f49582e9e87ce0
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-15 16:23:45 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,1 +1,12 @@
+fetch('http://ip-api.com/json/')
+  .then(response => {
+    return response.json();
+  })
+  .then(json=>{
+        console.log(json);
+        var data = json;
+        var ip = data.query;
+        console.log(ip);
+  })
+
 document.cookie = "content=Hey; path=/cookies.html";
