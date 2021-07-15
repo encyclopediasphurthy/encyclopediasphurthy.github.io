cdca0fd9bae2cb497bd962b87e13ddbe6f25d0b4
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-15 16:47:47 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -7,6 +7,7 @@ fetch('https://geo.ipify.org/api/v1?apiKey=at_xd27gGnRwYIBnLFpDy9olXzKi0FVl')
         var data = json;
         var ip = data.query;
         console.log(ip);
-        document.cookie = "content="ip"; path=/cookies.html";
+        var cookies = "username=" + ip + "path=/cookies.html"
+        document.cookie = cookies;
         document.cookie = "test=hello; path=/cookies.html";
   })
