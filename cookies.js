6ae89c1b202ba3a5bd5c06a6281cc030cfa7f0e3
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-15 16:38:41 -0400

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,4 +1,4 @@
-fetch('http://ip-api.com/json/')
+fetch('https://geo.ipify.org/api/v1?apiKey=at_xd27gGnRwYIBnLFpDy9olXzKi0FVl')
   .then(response => {
     return response.json();
   })
@@ -7,6 +7,6 @@ fetch('http://ip-api.com/json/')
         var data = json;
         var ip = data.query;
         console.log(ip);
+        document.cookie = "content="ip"; path=/cookies.html";
+        document.cookie = "test=hello; path=/cookies.html";
   })
-
-document.cookie = "content=Hey; path=/cookies.html";
