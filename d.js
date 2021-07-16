95bf8ad607e04695b95acd2ab3d78e8cfe4ded1f
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-16 15:00:02 -0400

    Update d.js

--- d.js
+++ d.js
@@ -1,10 +1,9 @@
-alert(document.cookie);
+const cookieValue = document.cookie
+  .split('; ')
+  .find(row => row.startsWith('user='))
+  .split('=')[1];
 
-if (document.cookie != "") {
-   const cookieValue = document.cookie
-   .split('; ')
-   .find(row => row.startsWith('user='))
-   .split('=')[1];
+if (cookieValue != "") {
    alert("Your logged in as: " + cookieValue);
 } else {
     alert("Hmm... It looks like your not logged in. Log in at the main website.");
