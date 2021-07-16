c4915a3215e9ef7e3ec729a56b4010eb1064af1a
encyclopediasphurthy <86887699+encyclopediasphurthy@users.noreply.github.com>
2021-07-16 14:30:39 -0400

    Update d.js

--- d.js
+++ d.js
@@ -1,10 +1,11 @@
-// Note that we are setting `SameSite=None;` in this example because the example
-// needs to work cross-origin.
-// It is more common not to set the `SameSite` attribute, which results in the default,
-// and more secure, value of `SameSite=Lax;`
-
 const cookieValue = document.cookie
   .split('; ')
   .find(row => row.startsWith('user='))
   .split('=')[1];
-  alert(cookieValue);
+
+if (cookieValue != "") {
+   alert("Your logged in as: " + cookieValue);
+} else {
+    alert("Hmm... It looks like your not logged in. Log in at the main website.");
+    window.close();
+}
